d20d67d4fcdeecaad992debdc63643758217a1e7
Ssony <Mcfreddyjesson@gmail.com>
2025-07-14 22:00:01 +0800

    fix: correct type in FIREBASE_CLIENT_EMAIL

--- chatgpt-clone/lib/auth.ts
+++ chatgpt-clone/lib/auth.ts
@@ -13,7 +13,7 @@ export const authOptions: AuthOptions = {
     adapter: FirestoreAdapter({
         credential: cert({
             projectId: process.env.FIREBASE_PROJECT_ID,
-            clientEmail: process.env.FIREBASE_CLIENT_EMAI,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
             privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
         }),
     }),
